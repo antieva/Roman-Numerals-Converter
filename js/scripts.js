725ad7539beffb420c2bf9716173183db9ede206
Dennise Ortega and Eva Antipina <dennise@>
2018-03-29 09:14:06 -0700

    Add initial files.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business logic
+
+var romanNumTranslator = function(str){
+  var arrOfNumbers = str.split("");
+  var romanNumerals = [I,V,X,L,C,D,M];
+  if (arrOfNumbers.length > 4){
+    alert("You cannot count higher than 3,999 in Roman numerals");
+    return;
+  }else if (arrOfNumbers.length == 1){
+    
+  }
+}
