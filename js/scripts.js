188ab75014a49587bd9efa8c7097b51b74ac64a0
Dennise Ortega and Eva Antipina <dennise@>
2018-03-29 11:21:40 -0700

    Complete business logic. It will convert any number less 4000 to Roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,70 @@
 // Business logic
+var convertion = function (num,str) {
+  var symbols = str.split(",");
+  var result = "";
+  if (symbols.length > 3){
+    return;
+  } else if (symbols.length === 1){
+    var i = num;
+    result = symbols[0];
+    while (i != 1){
+      result = result.concat(symbols[0]);
+      i --;
+    }
+    return result;
+  }
+  if (num === 1 || num === 2 || num === 3) {
+    var i = num;
+    result = symbols[0];
+    while (i != 1){
+      result = result.concat(symbols[0]);
+      i --;
+    }
+    return result;
+  } else if (num === 4){
+      result = symbols[0].concat(symbols[1]);
+      return result;
+  } else if (num === 5){
+      result = symbols[1];
+      return result;
+  } else if (num === 6 || num === 7 || num === 8) {
+    var i = num;
+    result = symbols[1];
+    while (i != 5){
+      result = result.concat(symbols[0]);
+      i --;
+    }
+    return result;
+  } else if (num === 9){
+      result = symbols[0].concat(symbols[2]);
+      return result;
+  }
+
+}
+
+
+
 
 var romanNumTranslator = function(str){
   var arrOfNumbers = str.split("");
-  var romanNumerals = ["I","V","X","L","C","D","M"];
+  var output = "";
+  //var romanNumerals = ["I","V","X","L","C","D","M"];
   if (arrOfNumbers.length > 4){
     alert("You cannot count higher than 3,999 in Roman numerals");
     return;
   }else if (arrOfNumbers.length == 1){
-    if (arrOfNumbers[0] === '1' || arrOfNumbers[0] === '2' || arrOfNumbers[0] === '3') {
-      var i = arrOfNumbers[0];
-      arrOfNumbers[0] = romanNumerals[0];
-      while (i != 1){
-        arrOfNumbers[0] = arrOfNumbers + romanNumerals[0];
-        i --;
-      }
-      return arrOfNumbers.join("");
-    } else if (arrOfNumbers[0] === '4'){
-        arrOfNumbers[0] = romanNumerals[0] + romanNumerals[1];
-        return arrOfNumbers.join("");
-    }
+    return output = convertion(parseInt(arrOfNumbers[0]), "I,V,X");
+  } else if (arrOfNumbers.length == 2){
+    output = convertion(parseInt(arrOfNumbers[0]), "X,L,C");
+    return output =output + convertion(parseInt(arrOfNumbers[1]), "I,V,X");
+  } else if (arrOfNumbers.length == 3){
+    output = convertion(parseInt(arrOfNumbers[0]), "C,D,M");
+    output = output + convertion(parseInt(arrOfNumbers[1]), "X,L,C");
+    return output = output + convertion(parseInt(arrOfNumbers[2]), "I,V,X");
+  } else if (arrOfNumbers.length == 4){
+    output = convertion(parseInt(arrOfNumbers[0]), "M");
+    output = output + convertion(parseInt(arrOfNumbers[1]), "C,D,M");
+    output = output + convertion(parseInt(arrOfNumbers[2]), "X,L,C");
+    return output = output + convertion(parseInt(arrOfNumbers[3]), "I,V,X");
   }
 }
