d4bc83d04a763562fa9152497e0a7dfd4dd8f7fc
Dennise Ortega and Eva Antipina <dennise@>
2018-03-29 09:47:15 -0700

    Add part of business logic in scripts.js. It will convert numbers from 1 to 4 to Roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,22 @@
 
 var romanNumTranslator = function(str){
   var arrOfNumbers = str.split("");
-  var romanNumerals = [I,V,X,L,C,D,M];
+  var romanNumerals = ["I","V","X","L","C","D","M"];
   if (arrOfNumbers.length > 4){
     alert("You cannot count higher than 3,999 in Roman numerals");
     return;
   }else if (arrOfNumbers.length == 1){
-    
+    if (arrOfNumbers[0] === '1' || arrOfNumbers[0] === '2' || arrOfNumbers[0] === '3') {
+      var i = arrOfNumbers[0];
+      arrOfNumbers[0] = romanNumerals[0];
+      while (i != 1){
+        arrOfNumbers[0] = arrOfNumbers + romanNumerals[0];
+        i --;
+      }
+      return arrOfNumbers.join("");
+    } else if (arrOfNumbers[0] === '4'){
+        arrOfNumbers[0] = romanNumerals[0] + romanNumerals[1];
+        return arrOfNumbers.join("");
+    }
   }
 }
