9f0f2871fbb607c4dd27cbbf231bbfaa781e358c
Eva Antipina <eva.antipina@gmail.com>
2018-03-30 17:21:12 -0700

    Change business logic, initialize aglobal object which used as a map for function converting a number into string of Roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,43 @@
-// Business logic
-var convertion = function (num,str) {
-  var symbols = str.split(",");
+// Business logic.
+
+// Initializing an object.
+var arr = {
+  "0": ["","","",""],
+  "1": ["M","C","X","I"],
+  "2": ["MM","CC","XX","II"],
+  "3": ["MMM","CCC","XXX","III"],
+  "4": ["","CD","XL","IV"],
+  "5": ["","D","L","V"],
+  "6": ["","DC","LX","VI"],
+  "7": ["","DCC","LXX","VII"],
+  "8": ["","DCCC","LXXX","VIII"],
+  "9": ["","CM","XC","IX"]
+}
+
+// The function takes an integer as a parameter and returns a string.
+var toRomans = function(number) {
   var result = "";
-  if (num < 0){
-    alert("Please enter positive number!");
-    return;
+  var order = 1000;
+  for (var i = 0; i < 4; i++){
+    // Using Math.floor to devide variables and get an integer as a result.
+    var j = Math.floor(number / order);
+    result += arr[j.toString()][i];
+    number %= order;
+    order /= 10;
   }
-  if (symbols.length > 3){
-    return;
-  } else if (symbols.length === 1){
-    var i = num;
-    result = symbols[0];
-    while (i != 1){
-      result = result.concat(symbols[0]);
-      i --;
-    }
-    return result;
-  }
-  if (num === 0){
-    return result = "";
-  } else if (num === 1 || num === 2 || num === 3) {
-    var i = num;
-    result = symbols[0];
-    while (i != 1){
-      result = result.concat(symbols[0]);
-      i --;
-    }
-    return result;
-  } else if (num === 4){
-      result = symbols[0].concat(symbols[1]);
-      return result;
-  } else if (num === 5){
-      result = symbols[1];
-      return result;
-  } else if (num === 6 || num === 7 || num === 8) {
-    var i = num;
-    result = symbols[1];
-    while (i != 5){
-      result = result.concat(symbols[0]);
-      i --;
-    }
-    return result;
-  } else if (num === 9){
-      result = symbols[0].concat(symbols[2]);
-      return result;
-  }
-
+  return result;
 }
 
 
 
-
-
-var romanNumTranslator = function(str){
-  var numbers = parseInt(str);
-  if (isNaN(numbers)){
-    alert("Please enter a number!");
-  } else if (numbers < 0 || numbers > 3999) {
-    alert("Please enter positive number. And you cannot count higher than 3,999 in Roman numerals.");
-    return;
-  }
-  var arrOfNumbers = str.split("");
-  var output = "";
-
-  if (arrOfNumbers.length == 1){
-    return output = convertion(parseInt(arrOfNumbers[0]), "I,V,X");
-  } else if (arrOfNumbers.length == 2){
-    output = convertion(parseInt(arrOfNumbers[0]), "X,L,C");
-    return output =output + convertion(parseInt(arrOfNumbers[1]), "I,V,X");
-  } else if (arrOfNumbers.length == 3){
-    output = convertion(parseInt(arrOfNumbers[0]), "C,D,M");
-    output = output + convertion(parseInt(arrOfNumbers[1]), "X,L,C");
-    return output = output + convertion(parseInt(arrOfNumbers[2]), "I,V,X");
-  } else if (arrOfNumbers.length == 4){
-    output = convertion(parseInt(arrOfNumbers[0]), "M");
-    output = output + convertion(parseInt(arrOfNumbers[1]), "C,D,M");
-    output = output + convertion(parseInt(arrOfNumbers[2]), "X,L,C");
-    return output = output + convertion(parseInt(arrOfNumbers[3]), "I,V,X");
-  }
-}
-
-//user interface logic starts here
+// UI logic starts here.
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
     $(".output").hide();
-    var userInput = $("#number").val();
+    var userInput = parseInt($("#number").val());
 
-    var output = romanNumTranslator(userInput);
+    var output = toRomans(userInput);
     $(".romanNumeral").text(output);
 
     $(".output").show();
